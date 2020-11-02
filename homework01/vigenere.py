29cc3c8f78b3e2a3cfadf877f9949075b08dd649
pashatheslayer <yanpasha97@gmail.com>
2020-11-03 00:45:57 +0300

    vigenere

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,7 +1,6 @@
 def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     """
     Encrypts plaintext using a Vigenere cipher.
-
     >>> encrypt_vigenere("PYTHON", "A")
     'PYTHON'
     >>> encrypt_vigenere("python", "a")
@@ -10,14 +9,36 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    alph = "abcdefghijklmnopqrstuvwxyz"
+    j = 0
+
+    for i in range(len(plaintext)):
+        if plaintext[i].isalpha():
+            if alph.index(plaintext[i].lower()) + alph.index(keyword[j].lower()) >= len(alph):
+                g = alph.index(plaintext[i].lower()) + alph.index(keyword[j].lower()) - len(alph)
+            else:
+                g = alph.index(plaintext[i].lower()) + alph.index(keyword[j].lower())
+            if plaintext[i].isupper():
+                ciphertext += alph[g].upper()
+            else:
+                ciphertext += alph[g]
+            if j == len(keyword) - 1:
+                j = 0
+            else:
+                j += 1
+        else:
+            ciphertext += plaintext[i]
+            if j == len(keyword) - 1:
+                j = 0
+            else:
+                j += 1
+
     return ciphertext
 
 
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     """
     Decrypts a ciphertext using a Vigenere cipher.
-
     >>> decrypt_vigenere("PYTHON", "A")
     'PYTHON'
     >>> decrypt_vigenere("python", "a")
@@ -26,5 +47,27 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    alph = "abcdefghijklmnopqrstuvwxyz"
+    j = 0
+
+    for i in range(len(ciphertext)):
+        if ciphertext[i].isalpha():
+            if alph.index(ciphertext[i].lower()) - alph.index(keyword[j].lower()) < 0:
+                g = alph.index(ciphertext[i].lower()) - alph.index(keyword[j].lower()) + len(alph)
+            else:
+                g = alph.index(ciphertext[i].lower()) - alph.index(keyword[j].lower())
+            if ciphertext[i].isupper():
+                plaintext += alph[g].upper()
+            else:
+                plaintext += alph[g]
+            if j == len(keyword) - 1:
+                j = 0
+            else:
+                j += 1
+        else:
+            plaintext += ciphertext[i]
+            if j == len(keyword) - 1:
+                j = 0
+            else:
+                j += 1
     return plaintext
